4191cabf828afd39947922779845e7dfa4be7bbf
marceloescobar <escobar.marcelo@gmail.com>
2024-03-21 20:43:06 -0300

    commit inicial

---
+++ src/test/java/com/mescobar/geoapi/GeoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mescobar.geoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
